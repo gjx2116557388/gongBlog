2f4c20904a7a7484bc74804975607f6350ccf800
代码酱 <2116557388@qq.com>
2022-12-01 10:22:29 +0800

    first commit

---
+++ gongBlog/src/App.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+    import { useDark, useToggle } from '@vueuse/core'
+    import { Sunny, Moon } from '@element-plus/icons-vue'
+    import { ref } from 'vue'
+
+    const isDark = useDark()
+    const toggleDark = useToggle(isDark)
+    
+    const activeIndex = ref('1')
+const activeIndex2 = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+
+<template>
+  <div>
+    <el-affix :offset="0">
+      <div>
+        <el-menu
+          :default-active="activeIndex"
+          mode="horizontal"
+          @select="handleSelect"
+          class="el-menu-demo"
+        >
+            <div>
+              <router-link to="/">
+                <!-- <el-image style="width: 100px; height: 50px" src="https://www.lovelilin.com/images/%E5%AF%8C%E5%BC%BA.png" fit="cover" /> -->
+              </router-link>
+            </div>
+            <div class="flex-grow" /> 
+              <el-menu-item index="1">首页</el-menu-item>
+              <el-menu-item index="2">
+              测试页
+                
+              </el-menu-item>
+              <el-menu-item index="3">测试</el-menu-item>
+              <el-menu-item index="4">关于</el-menu-item>
+              <div class="switch">
+                <el-switch
+                    v-model="isDark"
+                    style="--el-switch-on-color: #2C2C2C; --el-switch-off-color: #DCDFE6;color:red"
+                    size="large"
+                    inline-prompt
+                    :active-icon="Sunny"
+                    :inactive-icon="Moon"
+                />
+              </div>
+          </el-menu>
+      </div>
+    </el-affix>
+    <div class="main">
+     <div class="router-view">
+      <router-view></router-view>
+     </div>
+     <div class="info-wraps">
+      我是广告模块
+     </div>
+    
+    </div>
+   
+  </div>
+</template>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+}
+</style>
+<style scoped>
+  .switch{
+  /* display: flex; */
+  /* justify-content: center; */
+  align-items: center;
+  width: 200px;
+}
+.flex-grow {
+  flex-grow: 1;
+}
+.el-menu-demo{
+  padding: 0 10vw;
+}
+.main{
+  padding: 0 10vw;
+  height: auto;
+  display:flex;
+}
+.router-view{
+  width: 70%;
+  /* background:red; */
+}
+.info-wraps{
+  width: 30%;
+  margin:10px 10px 10px 200px;
+  /* background:pink; */
+  border:1px solid var(--el-menu-border-color);
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-height: 800px;
+}
+</style>
---
+++ gongBlog/src/pages/home.vue
@@ -0,0 +1,140 @@
+<template>
+    <div>
+        <div class="wrap">
+            <ul class="infinite-list" style="overflow: auto">
+                <li class="infinite-list-item" v-for="(item,index) in data[currentPage]" :key="index">
+                    <h2 style="color:var(--el-menu-text-color);">{{item.name}}</h2>
+                    <br />
+                    <!-- <h3>Lookah</h3> -->
+                    <div>
+                        <span style="color: #9aa0a6;">2022年10月30</span>
+                        <span style="color: var(--el-menu-text-color);">
+                           {{item.content}}
+                        </span>
+                    </div>
+                </li>
+            </ul>
+            <el-pagination background layout="prev, pager, next" :page-count="lengths" class='pagination' @current-change="currentPageFn" />
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+const count = ref(0)
+const currentPage = ref(0)
+const currentPageFn = (val) => {
+    currentPage.value = val - 1
+    console.log(val);
+    
+}
+// console.log(currentPage);
+
+const load = () => {
+  count.value += 2
+}
+
+const data = ref([
+    [   {name:"我是第一页",content:"测试分页功能是否正常"},
+        {name:"JavaScript百炼成仙",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"床前明月光",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"美人卷珠帘",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"我是第一页",content:"测试分页功能是否正常"},
+        {name:"JavaScript百炼成仙",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"床前明月光",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        
+    ],
+    [
+        {name:"JavaScript百炼成仙",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"床前明月光",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"美人卷珠帘",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+
+    ],
+    [
+        {name:"JavaScript百炼成仙",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"床前明月光",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"美人卷珠帘",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+
+    ],
+    [
+        {name:"JavaScript百炼成仙",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"床前明月光",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"美人卷珠帘",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+
+    ],
+    [
+        {name:"JavaScript百炼成仙",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"床前明月光",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"美人卷珠帘",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+
+    ],
+    [
+        {name:"JavaScript百炼成仙",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"床前明月光",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"美人卷珠帘",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+
+    ],
+    [
+        {name:"JavaScript百炼成仙",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"床前明月光",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"美人卷珠帘",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+
+    ],
+    [
+        {name:"JavaScript百炼成仙",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"床前明月光",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"美人卷珠帘",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+
+    ],
+    [
+        {name:"JavaScript百炼成仙",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"床前明月光",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"美人卷珠帘",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+
+    ],
+    [
+        {name:"JavaScript百炼成仙",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"床前明月光",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+        {name:"美人卷珠帘",content:' Lookah is an online headshop also known as a smoke shop offering premium design glass rigs, cool water pipes under our Lookah Glass and Tataoo Glass brands.'},
+
+    ],
+    
+])
+const lengths = data.value.length
+console.log(data.value.length);
+
+</script>
+<style scoped>
+    .wrap{
+        /* width:50%; */
+    }
+    .pagination{
+        /* display:flex; */
+        justify-content: center;
+        /* align-items: center; */
+    }
+    .infinite-list {
+        height: auto;
+        padding: 0;
+        margin: 0;
+        list-style: none;
+        }
+    /* .infinite-list  */
+    .infinite-list-item {
+        /* display: flex;
+        align-items: center;
+        justify-content: center; */
+        height: 10vh;
+        min-height: 150px;
+        /* width: 100%; */
+        min-width: 240px;
+        /* background: var(--el-color-primary-light-9); */
+        margin: 10px;
+        color: var(--el-color-primary);
+        box-shadow: 0 2px 4px #00000014;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    .infinite-list .infinite-list-item + .list-item {
+        margin-top: 10px;
+    }
+</style>
